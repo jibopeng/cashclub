bab9949c23a1bd99a9ff5d73815aadb2ce58a6c8
jbp <17600558224@163.com>
2021-07-26 16:16:28 +0800

    第一次提交项目

---
+++ loan-supermarket-app/src/test/java/com/example/loansupermarketapp/LoanSupermarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loansupermarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSupermarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ loan-supermarket-web/src/main/java/com/example/loansupermarketweb/LoanSupermarketWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.loansupermarketweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSupermarketWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanSupermarketWebApplication.class, args);
+    }
+
+}
